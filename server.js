44372e112e4cb608184559dbd9721e4e85257b97
riccardo preite <riccardo.preite.10@gmail.com>
2022-06-01 16:15:47 +0200

    push finished project

---
+++ server.js
@@ -0,0 +1,381 @@
+const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+var toWav = require("audiobuffer-to-wav");
+const app = express();
+const path = require("path");
+const youtubeSearch = require("youtube-search");
+const utils = require("./serverSideUtils.js");
+var client = require("mongodb").MongoClient;
+var parseString = require("xml2js").parseString;
+var https = require("https");
+var bodyParser = require("body-parser");
+const upload = multer();
+const Youtube = require("youtube-api"),
+  readJson = require("r-json"),
+  opn = require("opn"),
+  prettyBytes = require("pretty-bytes");
+const ffmpeg = require("fluent-ffmpeg");
+const Entities = require("html-entities").XmlEntities;
+
+const entities = new Entities();
+// app.use(bodyParser.urlencoded());
+
+const rickyKey = "AIzaSyBEpETjNZc18OP9L603YkzOvotslkQiBGI";
+const rickykey_second = "AIzaSyB5PLURpl92Ix6gBHvgBMJ9s1JC7m69b2c";
+const guiKey = "AIzaSyBFXSS4CBQKDc8yJtAdEruvXgAEHNwg8ko";
+const maxKey = "AIzaSyD5gNJnmZJlz4DsDcD1cFjgpqLfzX0LsFk";
+
+app.use(express.static("public")); // for serving the HTML file
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/askDBPedia", (req, res) => {
+  return new Promise(async (resolve, reject) => {
+    var vector = req.query.que.split(" ");
+    for (string in vector) {
+      vector[string] = "'" + vector[string] + "'";
+      vector[string] = vector[string].toUpperCase();
+    }
+    var string = req.query.que.toUpperCase();
+    string = string.replace(/ /g, " AND ");
+    var q =
+      "select ?s1 as ?c1, (bif:search_excerpt (bif:vector (" +
+      vector +
+      " , 'BOLOGNA'), ?o1)) as ?c2, ?sc, ?rank, ?g where {{{ select ?s1, (?sc * 3e-1) as ?sc, ?o1, (sql:rnk_scale (<LONG::IRI_RANK> (?s1))) as ?rank, ?g where  { quad map virtrdf:DefaultQuadMap { graph ?g { ?s1 ?s1textp ?o1 . ?o1 bif:contains  '(" +
+      string +
+      " AND BOLOGNA)'  option (score ?sc)  . } } } order by desc (?sc * 3e-1 + sql:rnk_scale (<LONG::IRI_RANK> (?s1)))  limit 1  offset 0 }}}";
+    console.log("Eccomi");
+    dps
+      .client()
+      .query(q)
+      .timeout(15000)
+      .asJson()
+      .then(r => {
+        console.log(r);
+        res.send(r);
+      })
+      .catch(e => {
+        console.log(e);
+        res.send(e);
+      });
+  });
+});
+
+app.post("/updateReview", (req, res) => {
+  client.connect(
+    "mongodb://localhost:27017/",
+    { useUnifiedTopology: true },
+    async function(error, db) {
+      var myquery = { _id: req.query.luogo, wr: req.query.wr };
+      var newvalues = {
+        $set: {
+          _id: req.query.luogo,
+          value: {
+            voto: req.query.voto,
+            descrizione: req.query.descrizione
+          },
+          clip: req.query.clip,
+          wr: req.query.wr,
+          rd: req.query.rd
+        }
+      };
+      var mydb = db.db("WMIdb");
+      mydb
+        .collection("review")
+        .updateOne(myquery, newvalues, function(err, result) {
+          if (err) {
+            res.status(400).send({
+              message: "DB error"
+            });
+          } else {
+            res.send("Update!");
+          }
+        });
+    }
+  );
+});
+
+app.get("/getReview", async (req, res) => {
+  client.connect(
+    "mongodb://localhost:27017/",
+    { useUnifiedTopology: true },
+    async function(error, db) {
+      if (req.query.mode == "user") {
+        if (error) {
+          res.status(400).send({
+            message: "DB error"
+          });
+        } else {
+          var mydb = db.db("WMIdb");
+          mydb
+            .collection("review")
+            .find({ wr: req.query.wr })
+            .toArray(function(err, result) {
+              if (err) {
+                res.status(400).send({
+                  message: "DB error"
+                });
+              } else {
+                if (result.length == 0) {
+                  res.send("null");
+                } else {
+                  res.send(result);
+                }
+              }
+            });
+        }
+      } else if (req.query.mode == "check") {
+        var mydb = db.db("WMIdb");
+        var ifExist = await utils.getSingleReview(req, mydb);
+        res.send(ifExist);
+      } else {
+        var mydb = db.db("WMIdb");
+        mydb
+          .collection("review")
+          .find({ rd: req.query.rd })
+          .toArray(function(err, result) {
+            if (err) {
+              res.status(400).send({
+                message: "DB error"
+              });
+            } else {
+              if (result.length == 0) {
+                res.send("null");
+              } else {
+                res.send(result);
+              }
+            }
+          });
+      }
+    }
+  );
+});
+
+app.post("/insertReview", (req, res) => {
+  client.connect(
+    "mongodb://localhost:27017/",
+    { useUnifiedTopology: true },
+    async function(error, db) {
+      if (!error) {
+        var mydb = db.db("WMIdb");
+
+        var ifExist = await utils.getSingleReview(req, mydb);
+        console.log("Eccomi");
+        if (ifExist == "error") {
+          res.status(400).send({
+            message: "DB error"
+          });
+        } else if (ifExist.length == 0) {
+          console.log("Pro");
+          var myobj = {
+            _id: req.query.luogo,
+            value: {
+              voto: req.query.voto,
+              descrizione: req.query.descrizione
+            },
+            clip: req.query.clip,
+            wr: req.query.wr,
+            rd: req.query.rd
+          };
+          mydb.collection("review").insertOne(myobj, function(err, result) {
+            if (err) {
+              res.status(400).send({
+                message: "DB error"
+              });
+            } else {
+              res.send("Insert!");
+            }
+          });
+        } else {
+          res.send(ifExist);
+        }
+      }
+      db.close();
+    }
+  );
+});
+
+async function cerca(c, opts, nextPageToken, r) {
+  return new Promise(async (resolve, reject) => {
+    youtubeSearch(c, opts, async (err, results, pageInfo) => {
+      if (err) {
+        res.send({ error: err.response.statusText });
+      } else {
+        var boh = await results;
+        for (var val in boh) {
+          r.push(results[val]);
+        }
+        var npt = await pageInfo;
+        nextPageToken = npt.nextPageToken;
+        resolve({ npt: nextPageToken, list: r });
+      }
+    });
+  });
+}
+
+app.get("/getPOIs", async (req, res) => {
+  try {
+    var c = req.query.searchQuery;
+    var r = [];
+    var nextPageToken = null;
+    console.log(c);
+    do {
+      var opts = (youtubeSearch.YouTubeSearchOptions = {
+        maxResults: 50,
+        key: rickyKey,
+        pageToken: nextPageToken
+      });
+      var ret = await cerca(c, opts, nextPageToken, r);
+      r = ret.list;
+      nextPageToken = ret.npt;
+    } while (nextPageToken != undefined);
+    console.log(r.length);
+    var data = await call(r, res);
+    res.send(data);
+  } catch (error) {}
+});
+
+function call(results, res) {
+  return new Promise(async (resolve, reject) => {
+    var POIs = {};
+    var list = [];
+    var counter = 0;
+    client.connect(
+      "mongodb://localhost:27017/",
+      { useUnifiedTopology: true },
+      async function(error, db) {
+        var mydb = db.db("smogDB");
+        for (var key in results) {
+          var item = results[key];
+          if ((val = utils.validator(item.description)) !== false) {
+            if (list.indexOf(val.plusCode) === -1) {
+              list.push(val.plusCode);
+              var c = await utils.getDescription(item.title, mydb, utils);
+              item.title = entities.decode(item.title);
+              console.log("TITOLOO " + item.title);
+              try {
+                POIs[counter] = {
+                  name: item.title,
+                  coords: val.coords,
+                  videoId: item.id,
+                  description: c[0].descrizione,
+                  img: c[0].urlImg,
+                  visited: false
+                };
+              } catch (error) {
+                POIs[counter] = {
+                  name: item.title,
+                  coords: val.coords,
+                  videoId: item.id,
+                  description: c.descrizione,
+                  img: c.urlImg,
+                  visited: false
+                };
+              }
+              counter++;
+            }
+          }
+        }
+        resolve(POIs);
+      }
+    );
+  });
+}
+
+app.post("/uploadFile", upload.single("file"), function(req, res) {
+  const fileName = req.body.fname;
+  const id = req.body.id;
+  const newPath = "user/" + id + "/upload/";
+  const filePath = newPath + fileName + ".mp3";
+  if (!fs.existsSync(newPath)) {
+    fs.mkdirSync(newPath);
+  }
+  fs.writeFileSync(filePath, req.file.buffer, error => {
+    if (error) {
+      console.error(error);
+      res.send("err");
+    } else {
+      res.send("end");
+      //here you can save the file name to db, if needed
+    }
+  });
+  var proc = new ffmpeg({ source: filePath })
+
+    .addInputOption("-loop", "1")
+    .addInputOption("-i", "firma.jpg")
+    .addOptions([
+      "-c:v libx264",
+      "-tune stillimage",
+      "-c:a aac",
+      "-b:a 192k",
+      "-pix_fmt yuv420p",
+      "-shortest"
+    ])
+    .on("start", function(commandLine) {
+      console.log("Spawned FFmpeg with command: " + commandLine);
+    })
+    .on("error", function(err) {
+      console.log("error: ");
+      console.log(err);
+    })
+    .on("end", function(err) {
+      if (!err) {
+        console.log("conversion Done");
+      }
+      utils.upload(fileName, id);
+      res.send("end");
+    })
+    .setDuration(req.body.etime)
+    .saveToFile(newPath + fileName + ".mkv");
+});
+
+app.post("/cutAudio", upload.single("file"), function(req, res) {
+  //, is still necessary to upload file?
+  utils.cutAudio(req, res);
+});
+
+app.post("/saveToken", function(req, res) {
+  utils.reload(req);
+  res.send("end");
+});
+
+app.post("/removeDir", function(req, res) {
+  utils.remove(req.query.id);
+  res.send("deleted");
+});
+
+app.get("*", (req, res) => {
+  var ext = path.extname(req.url);
+  if (
+    ext === ".css" ||
+    ext === ".html" ||
+    ext === ".js" ||
+    ext === ".jpg" ||
+    ext === ".png" ||
+    ext === ".woff" ||
+    ext === ".woff2" ||
+    ext === ".ttf" ||
+    ext === ".svg" ||
+    ext === ".eot"
+  ) {
+    res.sendFile(path.join(__dirname, "./" + req.url));
+  } else if (ext === ".ico") {
+    res.status(204).json({ nope: true });
+  } else if (ext === ".mp3") {
+    //audio
+    res.sendFile(path.join(__dirname, "./" + req.url));
+  }
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert")
+    },
+    app
+  )
+  .listen(8000, () => console.log("Gator app listening on port 8000!"));
---
+++ serverSideUtils.js
@@ -0,0 +1,441 @@
+/* function validator(d) {
+    var list = d.split(":");
+    var check =
+      list.length >= 3 &&
+      list[0].split("-").length == 3 &&
+      type.indexOf(list[1]) != -1 &&
+      language.indexOf(list[2]) != -1;
+    if (check) {
+      console.log(d);
+      if (list[3] != undefined) {
+        if (list[3].charAt(0) != "A" && list[3].charAt(0) != "P") {
+          var contentList = list[3].split("-");
+          for (var content in contentList) {
+            check = check && contents.indexOf(contentList[content]) != -1;
+          }
+          if (list[4] != undefined && list[4].charAt(0) == "A") {
+            var val = list[4].replace("A", "");
+            check = check && audience.indexOf(val) != -1;
+            if (list[5] != undefined && list[5].charAt(0) == "P") {
+              var val = list[5].replace("P", "");
+              try {
+                check = check && parseInt(val, 10) >= 0;
+              } catch (err) {
+                check = false;
+              }
+            }
+          } else if (list[4] != undefined && list[4].charAt(0) == "P") {
+            var val = list[4].replace("P", "");
+            try {
+              check = check && parseInt(val, 10) >= 0;
+            } catch (err) {
+              check = false;
+            }
+          }
+        } else if (list[3] != undefined && list[3].charAt(0) == "A") {
+          var val = list[3].replace("A", "");
+          check = check && audience.indexOf(val) != -1;
+          if (list[4] != undefined && list[4].charAt(0) == "P") {
+            var val = list[4].replace("P", "");
+            try {
+              check = check && parseInt(val, 10) >= 0;
+            } catch (err) {
+              check = false;
+            }
+          }
+        } else if (list[3] != undefined && list[3].charAt(0) == "P") {
+          var val = list[3].replace("P", "");
+          try {
+            check = check && parseInt(val, 10) >= 0;
+          } catch (err) {
+            check = false;
+          }
+        }
+      }
+    }
+    try {
+      if (check) {
+        var ocls = list[0].split("-");
+        return { coords: openLocationCode.decode(ocls[2]), plusCode: ocls[2] };
+      } else {
+        throw check;
+      }
+    } catch (err) {
+      return false;
+    }
+  }*/
+
+const OpenLocationCode = require("open-location-code").OpenLocationCode;
+const openLocationCode = new OpenLocationCode();
+const axios = require("axios");
+const fs = require("fs");
+var parseString = require("xml2js").parseString;
+var multer = require("multer");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+var readJson = require("r-json");
+const Youtube = require("youtube-api");
+const dps = require("dbpedia-sparql-client").default;
+path = require("path");
+ffmpeg.setFfmpegPath(ffmpegPath);
+var latinize = require("latinize");
+const Entities = require("html-entities").XmlEntities;
+const entities = new Entities();
+
+const type = ["why", "how", "what"];
+
+const language = ["ita", "eng", "deu", "fra", "esp"];
+
+const contents = [
+  "none",
+  "nat",
+  "art",
+  "his",
+  "flk",
+  "mod",
+  "rel",
+  "cui",
+  "spo",
+  "mus",
+  "mov",
+  "fas",
+  "shp",
+  "tec",
+  "pop",
+  "prs",
+  "oth"
+];
+
+const audience = ["gen", "pre", "elm", "mid", "scl"];
+
+const CREDENTIALS = readJson("./credential.json");
+var auth = Youtube.authenticate({
+  type: "key",
+  key: "AIzaSyCAXQP_4KlAztXqWzAOvjv7Pa7DWIUb42U"
+});
+
+let oauth = Youtube.authenticate({
+  type: "oauth",
+  client_id: CREDENTIALS.web.client_id,
+  client_secret: CREDENTIALS.web.client_secret
+});
+
+function UploadYoutube(myTitle, myDescription, myTags, myFileLocation) {
+  var req = Youtube.videos.insert(
+    {
+      resource: {
+        // Video title and description
+        snippet: {
+          title: myTitle,
+          description: myDescription,
+          tags: myTags
+        },
+        // I don't want to spam my subscribers
+        status: {
+          privacyStatus: "public"
+        }
+      },
+      // This is for the callback function
+      part: "snippet,status",
+
+      // Create the readable stream to upload the video
+      media: {
+        body: fs.createReadStream(myFileLocation)
+      }
+    },
+    (err, data) => {
+      console.log("this data");
+      if (err) {
+        console.log(err);
+      } else console.log(data);
+      // process.exit();
+    }
+  );
+  return req;
+}
+
+module.exports = {
+  validator: function validator(d) {
+    var list = d.split(":");
+    var check =
+      list.length >= 3 &&
+      list[0].split("-").length == 3 &&
+      type.indexOf(list[1]) != -1 &&
+      language.indexOf(list[2]) != -1;
+    if (check) {
+      console.log(d);
+      if (list[3] != undefined) {
+        if (list[3].charAt(0) != "A" && list[3].charAt(0) != "P") {
+          var contentList = list[3].split("-");
+          for (var content in contentList) {
+            check = check && contents.indexOf(contentList[content]) != -1;
+          }
+          if (list[4] != undefined && list[4].charAt(0) == "A") {
+            var val = list[4].replace("A", "");
+            check = check && audience.indexOf(val) != -1;
+            if (list[5] != undefined && list[5].charAt(0) == "P") {
+              var val = list[5].replace("P", "");
+              try {
+                check = check && parseInt(val, 10) >= 0;
+              } catch (err) {
+                check = false;
+              }
+            }
+          } else if (list[4] != undefined && list[4].charAt(0) == "P") {
+            var val = list[4].replace("P", "");
+            try {
+              check = check && parseInt(val, 10) >= 0;
+            } catch (err) {
+              check = false;
+            }
+          }
+        } else if (list[3] != undefined && list[3].charAt(0) == "A") {
+          var val = list[3].replace("A", "");
+          check = check && audience.indexOf(val) != -1;
+          if (list[4] != undefined && list[4].charAt(0) == "P") {
+            var val = list[4].replace("P", "");
+            try {
+              check = check && parseInt(val, 10) >= 0;
+            } catch (err) {
+              check = false;
+            }
+          }
+        } else if (list[3] != undefined && list[3].charAt(0) == "P") {
+          var val = list[3].replace("P", "");
+          try {
+            check = check && parseInt(val, 10) >= 0;
+          } catch (err) {
+            check = false;
+          }
+        }
+      }
+    }
+    try {
+      if (check) {
+        var ocls = list[0].split("-");
+        return { coords: openLocationCode.decode(ocls[2]), plusCode: ocls[2] };
+      } else {
+        throw check;
+      }
+    } catch (err) {
+      return false;
+    }
+  },
+  insertDescription: function insertDescription(mydb, titolo, desc, img) {
+    var myobj = { nome: titolo, descrizione: desc, urlImg: img };
+    mydb.collection("descrizioni").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+    });
+  },
+  askDBPedia: function(titolo) {
+    return new Promise((resolve, reject) => {
+      var vector = titolo.split(" ");
+      for (string in vector) {
+        vector[string] = "'" + vector[string] + "'";
+        vector[string] = vector[string].toUpperCase();
+      }
+      var string = titolo.toUpperCase();
+      string = string.replace(/&#([0-9]|[a-z])*;/g, "  ");
+      string = string.replace(/  +/g, " ");
+      string = string.replace(/ /g, " AND ");
+      string = latinize(string);
+      var q =
+        "select ?s1 as ?c1, (bif:search_excerpt (bif:vector (" +
+        vector +
+        " , 'BOLOGNA'), ?o1)) as ?c2, ?sc, ?rank, ?g where {{{ select ?s1, (?sc * 3e-1) as ?sc, ?o1, (sql:rnk_scale (<LONG::IRI_RANK> (?s1))) as ?rank, ?g where  { quad map virtrdf:DefaultQuadMap { graph ?g { ?s1 ?s1textp ?o1 . ?o1 bif:contains  '(" +
+        string +
+        " AND BOLOGNA)'  option (score ?sc)  . } } } order by desc (?sc * 3e-1 + sql:rnk_scale (<LONG::IRI_RANK> (?s1)))  limit 1  offset 0 }}}";
+      dps
+        .client()
+        .query(q)
+        .timeout(15000)
+        .asJson()
+        .then(r => {
+          resolve(r);
+        })
+        .catch(e => {
+          reject(e);
+        });
+    });
+  },
+
+  getDescription: function(nome, mydb, utils) {
+    return new Promise((resolve, reject) => {
+      var cont = {},
+        img;
+      console.log("Mmmmmmm");
+      mydb
+        .collection("descrizioni")
+        .find({ nome: nome })
+        .toArray(async function(err, result) {
+          if (result.length != 0) {
+            console.log("AHAHAHAHAH");
+            resolve(result);
+          } else {
+            try {
+              var d = await utils.askDBPedia(nome);
+              nome = entities.decode(nome);
+              console.log("QUAAA " + nome);
+              d =
+                d.results.bindings[0].c1.value.replace("resource", "data") +
+                ".rdf";
+              var e = await utils.get(d);
+              parseString(e.data, function(err, result) {
+                var json = {};
+                var list =
+                  result["rdf:RDF"]["rdf:Description"][0]["rdfs:comment"];
+                for (var key in list) {
+                  var chiave = list[key]["$"]["xml:lang"];
+                  var valore = list[key]["_"];
+                  json[chiave] = valore;
+                }
+                var img =
+                  result["rdf:RDF"]["rdf:Description"][0]["dbo:thumbnail"][0][
+                    "$"
+                  ]["rdf:resource"];
+                utils.insertDescription(mydb, nome, json, img);
+                cont["descrizione"] = json;
+                cont["urlImg"] = img;
+                resolve(cont);
+              });
+            } catch (error) {
+              nome = entities.decode(nome);
+              console.log("sooottoo " + nome);
+              var json = {};
+              var img = "NF";
+              json["en"] = "NOT FOUND";
+              cont["descrizione"] = json;
+              cont["urlImg"] = img;
+              utils.insertDescription(mydb, nome, json, img);
+              resolve(cont);
+            }
+          }
+        });
+    });
+  },
+  getSingleReview: async function(req, mydb) {
+    return new Promise((resolve, reject) => {
+      mydb
+        .collection("review")
+        .find({ _id: req.query.luogo, wr: req.query.wr, clip: req.query.clip })
+        .toArray(function(err, result) {
+          if (err) {
+            resolve("error");
+          } else {
+            resolve(result);
+          }
+        });
+    });
+  },
+
+  get: function get(search) {
+    return new Promise((resolve, reject) => {
+      resolve(axios.get(search));
+    });
+  },
+  dist: function dist(item, lat, lon) {
+    return new Promise((resolve, reject) => {
+      var url =
+        "https://graphhopper.com/api/1/matrix?from_point=" + lat + "," + lon;
+      url +=
+        "&to_point=" + item["latitudeCenter"] + "," + item["longitudeCenter"];
+      url +=
+        "&type=json&vehicle=foot&debug=true&out_array=weights&out_array=times&out_array=distances&key=a0695b22-2381-4b66-8330-9f213b610d8f";
+      axios
+        .get(url)
+        .then(response => {
+          item["distance"] = response["data"]["distances"][0][0];
+          resolve(response["data"]["distances"][0][0]);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  cutAudio: function cutAudio(audio, res) {
+    const fileName = audio.body.fname;
+    const stime = audio.body.stime;
+    const etime = audio.body.etime;
+    const id = audio.body.id;
+    const newPath = __dirname + "/user/" + id + "/";
+    const filePath = newPath + fileName.replace("Origin", "");
+
+    if (!fs.existsSync(newPath)) {
+      fs.mkdirSync(newPath);
+    }
+
+    fs.writeFileSync(filePath, audio.file.buffer, error => {
+      if (error) {
+        console.error(error);
+        res.end();
+      } else {
+        res.end(fileName);
+      }
+    });
+
+    if (fileName.includes("Origin")) {
+      console.log("E arrivato il file originale e lo salvo");
+      const origin = new ffmpeg({ source: filePath });
+      origin
+        .on("start", function(commandLine) {
+          console.log("Spawned FFmpeg with command: " + commandLine);
+        })
+        .on("error", function(err) {
+          console.log("errorp: ", +err.message);
+        })
+        .on("end", function(err) {
+          if (!err) {
+            console.log("conversion Done");
+          }
+        })
+        .saveToFile(newPath + "1" + fileName);
+    }
+    if (stime != undefined && etime != undefined) {
+      const conv = new ffmpeg({ source: filePath });
+      conv
+        .setStartTime(stime) //Can be in "HH:MM:SS" format also
+        .setDuration(etime - stime)
+        .on("start", function(commandLine) {
+          console.log("Spawned FFmpeg with command: " + commandLine);
+        })
+        .on("error", function(err) {
+          console.log("error: ", +err.message);
+        })
+        .on("end", function(err) {
+          if (!err) {
+            console.log("conversion Done");
+          }
+
+          x = 0;
+          res.send(
+            "https://localhost:8000/" + "user/" + id + "/new" + fileName
+          );
+        })
+        .saveToFile(newPath + "new" + fileName);
+    }
+  },
+  upload: function upload(fileName, id) {
+    const newPath = __dirname + "/user/" + id + "/upload/";
+    const absolute = newPath + fileName + ".mkv";
+    var result = UploadYoutube(
+      "prova2 title" + fileName,
+      "prova2 description" + fileName,
+      ["upload" + fileName, "prove2" + fileName],
+      absolute
+    );
+  },
+  reload: function reload(base) {
+    if (
+      oauth.credentials.access_token != undefined &&
+      oauth.credentials.refresh_token != undefined
+    ) {
+      oauth.setCredentials({
+        access_token: base.query.token,
+        refresh_token: base.query.refresh
+      });
+    }
+  },
+  remove: function remove(id) {
+    fs.rmdir(__dirname + "/user/" + id, { recursive: true }, function() {});
+  }
+};
